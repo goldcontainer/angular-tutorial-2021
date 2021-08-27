2e94e513f66d89f6c776e1b6eb91660127dc484a
goldcontainer <joshuabkim95@gmail.com>
2021-08-27 16:46:35 -0400

    added Task Components and Task Services

---
+++ angular-crash/src/app/components/task/task.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { TaskService } from '../../services/task.service';
+import { Task } from '../../Task';
+
+@Component({
+  selector: 'app-task',
+  templateUrl: './task.component.html',
+  styleUrls: ['./task.component.css']
+})
+export class TaskComponent implements OnInit {
+  tasks: Task[] = [];
+
+  constructor(private taskService: TaskService) {}
+
+  ngOnInit(): void {
+    this.taskService.getTasks().subscribe((tasks) => this.tasks = tasks);
+  }
+
+}
